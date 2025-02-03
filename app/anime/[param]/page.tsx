888b955256d30f106a64b56b88002b89495e1dd6
bilhaq12 <iboysgntg@gmail.com>
2025-02-03 23:08:37 +0700

    Update page.tsx

--- app/anime/[param]/page.tsx
+++ app/anime/[param]/page.tsx
@@ -1,9 +1,14 @@
 import { getEpisodeDetail } from "@/lib/api"
 import { EpisodeDetailClient } from "./episode-detail-client"
-import type { EpisodeDetail } from "@/types/anime"
+import type { EpisodeDetail } from "@/types/anime" // Assuming you have this type defined
 
-export default async function EpisodeDetailPage({ params }: { params: { param: string } }) {
-  const episodeDetail: EpisodeDetail = await getEpisodeDetail(params.param)
+interface PageProps {
+  params: Promise<{ param: string }>
+}
+
+export default async function EpisodeDetailPage({ params }: PageProps) {
+  const resolvedParams = await params
+  const episodeDetail: EpisodeDetail = await getEpisodeDetail(resolvedParams.param)
 
   return <EpisodeDetailClient initialEpisodeDetail={episodeDetail} />
 }
