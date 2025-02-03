76d461641b0d61785775200e5f91987a983a31ca
bilhaq12 <iboysgntg@gmail.com>
2025-02-04 00:40:27 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { AnimeList } from "@/components/anime-list";
 import { FeaturedCollection } from "@/components/featured-collection";
 import { getAnimeList } from "@/lib/api";
 
+
 export default async function Home({ searchParams }: { searchParams: { page?: string } }) { // Inline type
   const page = Number(searchParams.page) || 1;
   let animeList;
