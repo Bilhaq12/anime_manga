8c6fed8a210ae82fd4b6c73169660322164b63ee
bilhaq12 <iboysgntg@gmail.com>
2025-02-03 22:34:52 +0700

    Update page.tsx

--- app/anime/[param]/page.tsx
+++ app/anime/[param]/page.tsx
@@ -1,8 +1,22 @@
-import { getEpisodeDetail } from "@/lib/api"
-import { EpisodeDetailClient } from "./episode-detail-client"
+import { getEpisodeDetail } from "@/lib/api";
+import { EpisodeDetailClient } from "./episode-detail-client";
+import { notFound } from 'next/navigation';
+
+async function fetchEpisodeDetail(param: string) {
+    try {
+      const episodeDetail = await getEpisodeDetail(param);
+      if (!episodeDetail) {
+        notFound();
+      }
+      return episodeDetail;
+    } catch (error) {
+      console.error("Error fetching episode details:", error);
+      throw error;
+    }
+  }
 
 export default async function EpisodeDetailPage({ params }: { params: { param: string } }) {
-  const episodeDetail = await getEpisodeDetail(params.param)
+  const episodeDetail = await fetchEpisodeDetail(params.param);
 
-  return <EpisodeDetailClient initialEpisodeDetail={episodeDetail} />
+  return <EpisodeDetailClient initialEpisodeDetail={episodeDetail} />;
 }
